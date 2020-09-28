68d8a63f6edf91047d817d25f4751cb3e083d44a
testnight <www@github.com.cn>
2020-09-28 16:44:46 +0800

    thrid commit

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -2,13 +2,14 @@ package com.test;
 
 import com.test.dao.impl.UserDaoImpl;
 import com.test.service.Impl.UserServiceImpl;
-import com.test.service.UserService;
 
+/**
+ * @author Cyistal
+ */
 public class Main {
 
-
     public static void main(String[] args) {
-	    // write your code here
+
         UserServiceImpl userServiceImpl = new UserServiceImpl();
         userServiceImpl.setUserDao(new UserDaoImpl());
         userServiceImpl.getUserService();
